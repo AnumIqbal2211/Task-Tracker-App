df71708e60823ce31c2a0e6d0997ecb4200a97d1
Anum Iqbal <ianum0164@gmail.com>
2024-09-08 01:36:27 +0500

    Add TaskInput component for adding tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import React, {useState} from "react";
+import TaskInput from "./components/TaskInput";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
   return (
     <div className="App">
       <h1>Task Tracker App</h1>
+      <TaskInput addTask={addTask} />
     </div>
   );
 }
